4e336a55dee638d7d01e6217ec51382cede8abaa
Velocity-wap <swapnilbaruah2306@gmail.com>
2026-05-30 23:43:46 +0530

    Added ADC driver

--- Embedded/src/ADC.c
+++ Embedded/src/ADC.c
@@ -11,12 +11,20 @@ uint32_t *ADC_CR2 = (uint32_t *)(0x40012400 + 0x08);
 uint32_t *ADC_SR = (uint32_t *)(0x40012400 + 0x00);
 uint32_t *ADC_DR = (uint32_t *)(0x40012400 + 0x4C);
 
-int main(){
-    ADC_init();
-}
-
 void ADC_init(){
     *APB2ENR |= (1<<9) | (1<<2);
     *GPIOA_CRL &= ~(0xf<<0);
     *RCC_CFGR &= ~(0x00 << 0);
+    *ADC_CR2 |= (1<<0);
+    for(int i = 0; i<100; i++);
+    *ADC_SMPR2 |= (0x2<<0);
+    *ADC_SQR3 &= ~(0x1f << 0);
+    *ADC_SQR3 |= (0<<0);
+    *ADC_CR2 |= (1<<22);
+    while(!(*ADC_SR & (1<<1)));
+    uint16_t adc = *ADC_DR;
+}
+
+int main(){
+    ADC_init();
 }
