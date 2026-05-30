58e6993f424fa1dc6902b5a9dcf576ec41299d40
Velocity-wap <swapnilbaruah2306@gmail.com>
2026-05-30 19:24:24 +0530

    Started working on ADC Driver, Added memory addresses

--- Embedded/src/ADC.c
+++ Embedded/src/ADC.c
@@ -1,1 +1,22 @@
 #include <stdint.h>
+
+uint32_t *APB2ENR = (uint32_t *)(0x40021000 + 0x18);
+uint32_t *ADC1 = (uint32_t *)(0x40012400);
+uint32_t *GPIOA_CRL = (uint32_t *)(0x40010800 + 0x00);
+uint32_t *RCC_CFGR = (uint32_t *)(0x40021000 + 0x04);
+uint32_t *ADC_CR1 = (uint32_t *)(0x40012400 + 0x04);
+uint32_t *ADC_SMPR2 = (uint32_t *)(0x40012400 + 0x10);
+uint32_t *ADC_SQR3 = (uint32_t *)(0x40012400 + 0x34);
+uint32_t *ADC_CR2 = (uint32_t *)(0x40012400 + 0x08);
+uint32_t *ADC_SR = (uint32_t *)(0x40012400 + 0x00);
+uint32_t *ADC_DR = (uint32_t *)(0x40012400 + 0x4C);
+
+int main(){
+    ADC_init();
+}
+
+void ADC_init(){
+    *APB2ENR |= (1<<9) | (1<<2);
+    *GPIOA_CRL &= ~(0xf<<0);
+    *RCC_CFGR &= ~(0x00 << 0);
+}
